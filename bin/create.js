277471eb184133e272bc858337d3110fc8a46b17
Guilherme Diego Albino Francisco <guidiego.expgames@gmail.com>
2015-11-17 22:11:55 -0200

    Remove git after download

--- bin/create.js
+++ bin/create.js
@@ -8,11 +8,15 @@ module.exports = function (args, lib)  {
   }
 
   if (args[1] === "rest") {
+    console.log(color.bold.magenta('Cloning the AstronautREPO!'));
     exec('git clone https://github.com/astronautjs/astronaut-rest.git REST-JD23J238HF2309H2F')
     mv('REST-JD23J238HF2309H2F', args[2]);
     cd(args[2]);
+    console.log(color.bold.magenta('Install NPM Dependencies'));
     exec('npm install');
+
     exec('rm -Rf REST-JD23J238HF2309H2F');
+    exec('rm -Rf .git');
     console.log(color.bold.magenta('Thanks for using Astronaut! To infinity and beyond'));
   } else {
     console.log(color.red('Command not found try --help'));
